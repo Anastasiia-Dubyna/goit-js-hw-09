c56dbd8632d6e447275bd010004166c97cadd6f1
Anastasiia-Dubyna <122117575+Anastasiia-Dubyna@users.noreply.github.com>
2023-06-01 19:28:36 +0000

    Deploying to gh-pages from @ Anastasiia-Dubyna/goit-js-hw-09@800de75391fe91c4ba44735d8dff433cddfbd33b 🚀

---
+++ 01-color-switcher.1e435c82.js
@@ -0,0 +1,2 @@
+let t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=null;t.addEventListener("click",(function(a){t.disabled=!0,d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),e.disabled=!1})),e.addEventListener("click",(function(a){clearInterval(d),t.disabled=!1,e.disabled=!0})),e.disabled=!0;
+//# sourceMappingURL=01-color-switcher.1e435c82.js.map
